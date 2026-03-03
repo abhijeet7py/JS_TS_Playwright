d724e26dd9533fa5446d980c8f5ee628c3289092
abhijeet7py <abhijeetjathar709@gmail.com>
2026-03-03 08:42:03 +0530

    Add javascript folder properly

---
+++ javascript/basics1.js
@@ -0,0 +1,32 @@
+console.log("Hello World!");
+
+// This is single line comment
+
+/*
+
+*/
+
+let a = 4
+console.log(a)
+
+console.log(typeof(a))
+
+let b = 234.12
+console.log(typeof(b))
+
+let c = "Abhijeet"
+console.log(typeof(c))
+
+let d = true
+console.log(typeof(d))
+
+// let c = a + b (It will not work because We cannot redclare a variable with let keyword but we can with var)
+
+c = a + b // It will work because we are not we are just reassigning a new value to it
+console.log(c)
+
+// We cannot redclare a variable with let keyword but we can with var
+
+console.log(!d)
+
+// ! is not operator which gives the opposite of the boolean value
---
+++ javascript/basics2.js
@@ -0,0 +1,26 @@
+const flag = true
+
+if(!flag)
+{
+console.log("Condition is satisfied!")
+
+}
+else
+{
+console.log(flag)
+console.log("Condition is not satisfied!")
+}
+
+i = 0
+while(i >= 10)
+{
+    console.log(i)
+    i++
+}
+
+
+do
+{
+    i ++
+}while(i > 10);
+console.log(i)
