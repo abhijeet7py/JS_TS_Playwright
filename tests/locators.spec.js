f45a82c6a434a48ecf1a8138ab94e8a2ac79d65d
abhijeet7py <abhijeetjathar709@gmail.com>
2026-04-06 21:43:01 +0530

    Learning Playwright

--- tests/locators.spec.js
+++ tests/locators.spec.js
@@ -1,6 +1,6 @@
 const {test, expect} = require('@playwright/test');
 
-test.only("Locators in Playwright", async ({ browser, page }) =>
+test("Locators in Playwright", async ({ browser, page }) =>
     {
     // const context = await browser.newContext();
     //const page = await context.newPage();
@@ -12,13 +12,37 @@ test.only("Locators in Playwright", async ({ browser, page }) =>
     // it is faster than Xpath selector.
 
     // fill() method is used to fill the input field with the specified value.
-    await page.locator("#username").fill("rahulshettyacademy");
+    await page.locator("#username").fill("rahulshetty");
     await page.locator("[type='password']").fill("Learning@830$3mK2");
 
     // click() method is used to click on the element.
     await page.locator("#signInBtn").click();
 
+    // textContent() method is used to get the text content of the element.
+    console.log(await page.locator("[style *= 'block']").textContent())
 
+    // Assertion to check the error message
 
+    await expect(page.locator("[style *= 'block']")).toContainText("Incorrect");
 
 });
+
+test.only("Locators in playwright with multiple elements", async ({browser, page}) =>{
+
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+    const userName = page.locator("#username");
+    const password = page.locator("[type='password']");
+    const signInBtn = page.locator("#signInBtn");
+
+    await userName.fill("rahulshettyacademy");
+    await password.fill("Learning@830$3mK2");
+    await signInBtn.click();
+
+    // to get the count of the elements
+    console.log(await page.locator(".card-body a").count());
+    // to get the text content of the first element
+    console.log(await page.locator(".card-body a").first().textContent());
+    // to get the text content of the nth element
+    console.log(await page.locator(".card-body a").nth(1).textContent());
+})
